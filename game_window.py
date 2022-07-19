d1d3bd2fd0bf620ee36a7f977ed35fdaf5436b51
Afshin Babaei <afshinbabaei1997@gmail.com>
2022-07-19 20:56:00 +0200

    inherits now from screen

--- game_window.py
+++ game_window.py
@@ -13,25 +13,21 @@ image_gun_player = pygame.image.load("images/Gun_01.png")
 
 class Game_window(Map):
     file = 'maps/background.csv'
+    pygame.init()
 
     def __init__(self):
-
         self.load_background(self.file)
         self.game()
 
     # the game loop
     def game(self):
-        go = True
-        player = PlayerRobot((200, 200), 4, 1, 0.02, image_robot_player, image_gun_player)  # create a player_robot
+        run = True
+        player = PlayerRobot((200, 200), 4, 2, 0.02, image_robot_player, image_gun_player)  # create a player_robot
 
-        while go:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+        while run:
+            self.close_event()
             self.render_background()
             player.move_robot()
             player.draw_robot(self.screen)
-            pygame.display.update()
-
-            self.clock.tick(self.FPS)
+            self.update_screen()
+        pygame.quit()
