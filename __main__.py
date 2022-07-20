9d16b365914bfd970daa4f66e8def8f4e4d3d065
graue-gans <meex.kehrer@gmail.com>
2022-07-20 21:42:44 +0200

    restructured main file

--- __main__.py
+++ __main__.py
@@ -1,16 +1,5 @@
 from main_menu import Main_menu
 
-#start the game from this file
-class Main():
-
-    def __init__(self):
-        self.start_game()
-
-    def start_game(self):
-        menu = Main_menu()
-
-
-
 
 if __name__ == "__main__":
-    m = Main()
+    game = Main_menu()
