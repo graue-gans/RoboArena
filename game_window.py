38bb14d6c94d03af1e91eeebeb4d6b85416d6870
Afshin Babaei <afshinbabaei1997@gmail.com>
2022-08-04 18:49:56 +0200

    added the wather in the game and water collision

--- game_window.py
+++ game_window.py
@@ -3,81 +3,61 @@ import csv
 import pygame
 import sys
 
-
 from map_utility import Map
+
 from movement_utility import rot_center
 
-from robot import EnemyRobot, PatrolRobot, PlayerRobot
+from robot import PlayerRobot
 from upload_effects import player_robot_img, player_gun_img, explosion_effect
-
-
-image_robot = pygame.image.load("images/robot.png")
-image_gun = pygame.image.load("images/Gun_01.png")
+from weather import Rain, Snow
 
 
 class Game_window(Map):
     image_robot_player = player_robot_img
     image_gun_player = player_gun_img
 
-
     file = 'maps/background.csv'
     pygame.init()
 
-    def __init__(self):
+    def __init__(self, weather):
         super().__init__()
         self.load_background(self.main_map)
+        self.weather = weather
         self.game()
-
-    def init_enemy_robots(self):
-        # init the enemy robots that are there from the start
-        # e1 = EnemyRobot((810, 140), 180, image_robot, image_gun)
-        return [] 
-
-    def init_patrol_robots(self):
-        p1 = PatrolRobot((810, 140), 2, 2, 0.02, image_robot, image_gun, (0, 1000), (0, 1000))
-        return [p1]
 
     # the game loop
     def game(self):
         run = True
-
-        player = PlayerRobot((100, 100), 2, 2, 0.02, self.image_robot_player, self.image_gun_player)  # create a player_robot
+        player = PlayerRobot((200, 200), 2, 2, 0.02)  # create a player_robot
         counter = 0
-        enemies = self.init_enemy_robots()
-        patrols = self.init_patrol_robots()
-        passed = False
-        t = 0
-
+        rain = [Rain() for i in range(300)]
+        snow = [Snow() for i in range(300)]
         while run:
             counter %= 60
-            self.close_event()
-            self.render_background(self.screen)
-            player.colision_check_lava(self.lava_mask())
-            player.colision_check_wall(self.wall_mask(), counter)
-            player.move_robot()
-
-            # static enemy robots:
-            for bot in enemies:
-                if t > 3000:
-                    bot.act(self.screen)
-                    passed = True
-                bot.move_robot()
-                bot.draw(self.screen, counter)
-                bot.update_projectiles(self.screen)
-            if passed:
-                t = 0
-                passed = False
-            # patrol robots:
-            for bot in patrols:
-                bot.move_robot()
-                bot.draw(self.screen, counter)
-                bot.update_projectiles(self.screen)
-
-            player.draw(self.screen,counter)
             counter += 1
 
-            dt = self.update_screen()
-            t += dt
-            
+            self.close_event()
+            self.render_background(self.screen,counter)
+
+            player.check_wall_collision(self.wall_mask())
+
+
+            player.move_robot()
+
+            player.draw(self.screen, counter)
+            player.check_water_collision(self.water_mask()[0], self.water_mask()[1], self.screen)
+            player.check_lava_collision(self.lava_mask(), self.screen, counter)
+
+            if self.weather == "rain":
+                for i in range(Rain.start_rain(len(rain))):
+                    rain[i].show()
+                    rain[i].update()
+            if self.weather == "snow":
+                for i in range(Snow.start_snow(len(snow))):
+                    snow[i].show()
+                    snow[i].update()
+
+
+            self.update_screen()
 
         pygame.quit()
