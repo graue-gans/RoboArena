c4f218ef377c576793cbf44617b372d92b93ac88
Afshin Babaei <afshinbabaei1997@gmail.com>
2022-08-04 18:45:47 +0200

    new images and images from each corner of the robot for the collision

--- upload_effects.py
+++ upload_effects.py
@@ -1,17 +1,23 @@
 import pygame
 
 
-def scale_img(img,factor):
-    size = round(img.get_width() * factor) , round(img.get_height()*factor)
-    return pygame.transform.scale(img,size)
+def scale_img(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
 
 
-player_robot_img = scale_img(pygame.image.load("images/tank/Hull_04.png"), 0.4)
-player_gun_img = scale_img(pygame.image.load("images/Gun_01.png"),0.5)
+def load(filename):
+    image = pygame.image.load(filename)
+    return image
 
+player_robot_img = scale_img(load("images/tank_player/Hull_01.png"), 0.3)
+top_left = scale_img(load("images/tank_player/collision_check/top_left.png"), 0.3)
+top_right = scale_img(load("images/tank_player/collision_check/top_right.png"), 0.3)
+bottom_left = scale_img(load("images/tank_player/collision_check/bottom_left.png"), 0.3)
+bottom_right = scale_img(load("images/tank_player/collision_check/bottom_right.png"), 0.3)
+player_gun_img = scale_img(load("images/Weapon_Color_A/Gun_04.png"), 0.5)
 
-
-explosion_effect = [scale_img(pygame.image.load("images/explosion/Explosion_E.png"),0.5),
-                    scale_img(pygame.image.load("images/explosion/Explosion_F.png"),0.5),
-                    scale_img(pygame.image.load("images/explosion/Explosion_G.png"),0.5),
-                    scale_img(pygame.image.load("images/explosion/Explosion_H.png"),0.5)]
+explosion_effect = [scale_img(pygame.image.load("images/explosion/Explosion_E.png"), 0.5),
+                    scale_img(pygame.image.load("images/explosion/Explosion_F.png"), 0.5),
+                    scale_img(pygame.image.load("images/explosion/Explosion_G.png"), 0.5),
+                    scale_img(pygame.image.load("images/explosion/Explosion_H.png"), 0.5)]
