8734ebb9b56356b5e0517970ce646f2dffa19885
Afshin Babaei <afshinbabaei1997@gmail.com>
2022-08-05 12:31:12 +0200

    renamed some images

--- upload_effects.py
+++ upload_effects.py
@@ -15,7 +15,7 @@ top_left = scale_img(load("images/tank_player/collision_check/top_left.png"), 0.
 top_right = scale_img(load("images/tank_player/collision_check/top_right.png"), 0.3)
 bottom_left = scale_img(load("images/tank_player/collision_check/bottom_left.png"), 0.3)
 bottom_right = scale_img(load("images/tank_player/collision_check/bottom_right.png"), 0.3)
-player_gun_img = scale_img(load("images/Weapon_Color_A/Gun_04.png"), 0.5)
+player_gun_img = scale_img(load("images/Weapon_Color_A/Gun_04.png"), 0.4)
 
 explosion_effect = [scale_img(pygame.image.load("images/explosion/Explosion_E.png"), 0.5),
                     scale_img(pygame.image.load("images/explosion/Explosion_F.png"), 0.5),
