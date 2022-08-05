d2e1b5eda87ed4b1135da252932910336e8d3c7e
Afshin Babaei <afshinbabaei1997@gmail.com>
2022-08-05 12:33:28 +0200

    implemented a new rotation function for the gun

--- movement_utility.py
+++ movement_utility.py
@@ -6,7 +6,7 @@ import pygame
 #
 
 
-# rotate a image around it's center
+# rotate a image around it's center but returns the rotated image (needed for the collision)
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
     orig_rect = image.get_rect()
@@ -15,6 +15,14 @@ def rot_center(image, angle):
     rot_rect.center = rot_image.get_rect().center
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
+
+#rotate a image and blit it on the screen (needed for the Gun)
+def rot_center_gun(image, pos, angle,win):
+    """rotate an image while keeping its center"""
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = rot_image.get_rect(center=image.get_rect(topleft=pos).center)
+    win.blit(rot_image,rot_rect.topleft)
+
 
 
 
